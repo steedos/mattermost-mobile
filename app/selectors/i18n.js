af1603865685cb049a21f0876657b9ea1abc9acc
Jack Zhuang <steedos@users.noreply.github.com>
2019-04-30 23:53:19 +0800

    mobile default locale

--- app/selectors/i18n.js
+++ app/selectors/i18n.js
@@ -8,7 +8,17 @@ import {getCurrentUserLocale} from 'mattermost-redux/selectors/entities/i18n';
 
 // Not a proper selector since the device locale isn't in the redux store
 export function getCurrentLocale(state) {
-    const deviceLocale = DeviceInfo.getDeviceLocale().split('-')[0];
+    let deviceLocale = DeviceInfo.getDeviceLocale();
+    switch (deviceLocale) {
+    case 'zh-Hans-CN':
+        deviceLocale = 'zh-CN';
+        break;
+    case 'zh-Hans-TW':
+        deviceLocale = 'zh-TW';
+        break;
+    default:
+        deviceLocale = deviceLocale.split('-')[0];
+    }
     const defaultLocale = deviceLocale || DEFAULT_LOCALE;
 
     return getCurrentUserLocale(state, defaultLocale);
